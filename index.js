8db013ea72462d7705ea20b9ffc33e16cbfcfb1c
Jack Ramsay <115435163+vpb11@users.noreply.github.com>
2025-06-24 12:52:50 +0100

    fixed origin bs

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const axios = require("axios");
 const app = express();
+const cors = require("cors");
 const port = 80;
+
+app.use(cors({ origin: ["*"] }));
 
 app.get("/proxy-rotation/:first/:seccond/:proxy_id", async (req, res) => {
   const URL = `https://elusiveproxy.com/proxy-rotate/${req.params.first}/${req.params.seccond}/${req.params.proxy_id}`;
